3b4c1cc126ade040fc0286dd8bcc4ee6110dc7d6
chengfan <chengfan@greenet.net.cn>
2018-10-26 14:14:48 +0800

    init spring boot project

---
+++ src/main/java/com/chengfan/practice/gitlabshower/GitlabShowerApplication.java
@@ -0,0 +1,12 @@
+package com.chengfan.practice.gitlabshower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabShowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabShowerApplication.class, args);
+	}
+}
